f1b4540dc5cff0d3449d6828b479ba537db52fd4
MarcioHideo <marc-ishi@usp.br>
2025-06-05 23:02:39 -0300

    adjument for the constant values

---
+++ coffee_config.py
@@ -0,0 +1,35 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+            # Espresso typically doesn't need milk
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+RESOURCE = {
+    "ingredients": {
+        "water": 300,
+        "milk": 200,
+        "coffee": 100,
+    },
+    "money": 20
+}
